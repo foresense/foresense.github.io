6169de9c88379d157d8971a0ed7c886bb0d0a41d
Robert Beenen <robert@foresen.se>
2024-11-29 16:29:41 +0100

    updated flake.nix with treefmt

--- flake.nix
+++ flake.nix
@@ -1,62 +1,69 @@
 {
   description = "foresense website";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
     process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import inputs.systems;
-      imports = with inputs; [ process-compose-flake.flakeModule ];
-
+      imports = with inputs; [
+        process-compose-flake.flakeModule
+        treefmt-nix.flakeModule
+      ];
       perSystem =
-        {
-          pkgs,
-
-          ...
-        }:
-        let
-          python-environment = pkgs.python312.withPackages (
-            pp: with pp; [
-              ipython
-              ipykernel
-              django
-              django-stubs
-            ]
-          );
-        in
+        { pkgs, ... }:
         {
           devShells = {
             default = pkgs.mkShell {
               name = "foresense";
               nativeBuildInputs = [ ];
               packages = with pkgs; [
-                python-environment
                 live-server
                 superhtml
               ];
             };
           };
-
           process-compose = {
             foresense = {
               settings = {
-                environment =
-                  {
-                  };
+                environment = { };
                 processes = {
-                  live-server.command = "${pkgs.live-server}/bin/live-server --hard --open";
+                  live-server.command = "${pkgs.live-server}/bin/live-server --open";
                 };
               };
             };
           };
-
-          # formatter =
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+              deno = {
+                enable = true;
+                excludes = [ "*.html" ];
+              };
+            };
+            settings.formatter = {
+              nixfmt = {
+                options = [
+                  "--strict"
+                  "--verify"
+                ];
+              };
+              superhtml = {
+                command = "${pkgs.superhtml}/bin/superhtml";
+                includes = [ "*.html" ];
+                options = [ "fmt" ];
+              };
+            };
+          };
         };
     };
 }
